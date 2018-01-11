e580bf4b18dc9ca4dd22b0a083f10b2cf44c0a52
rajesh.pillai <rajesh.pillai@kohls.com>
2018-01-11 01:54:52 -0800

    Feature: Added simple routes

--- server.js
+++ server.js
@@ -1,5 +1,22 @@
-var myExpress = require("./myexpress.js");
+var myExpress = require("./lib/myexpress.js");
 
 var app = myExpress();
+
+app.get("/greeting", function (req, res) {
+    var message = "Good morning";
+    res.setHeader("Content-Type", "text/plain");
+    res.setHeader("Content-Length", message.length);
+    res.writeHead(200);
+    res.end(message);
+});
+
+app.get("/logoff", function (req, res) {
+    var message = "Signing off.  Good night.";
+    res.setHeader("Content-Type", "text/plain");
+    res.setHeader("Content-Length", message.length);
+    res.writeHead(200);
+    res.end(message);
+});
+
 
 app.listen(3000);
